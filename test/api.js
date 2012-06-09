b557fb56cdcbd1c023e646bd072e262216816958
James Halliday <mail@substack.net>
2012-06-09 04:49:58 -0700

    make the api test fail with an empty directory

--- test/api.js
+++ test/api.js
@@ -1,10 +1,13 @@
 var test = require('tap').test;
 var yarnify = require('yarnify');
 
+var mkdirp = require('mkdirp');
+mkdirp.sync(__dirname + '/widget/empty');
+
 test('api mode', function (t) {
     t.plan(3);
     
-    var dirs = [ 'css', 'html' ].map(function (dir) {
+    var dirs = [ 'css', 'html', 'empty' ].map(function (dir) {
         return __dirname + '/widget/' + dir;
     });
     yarnify.knit(dirs, function (err, bundle) {
